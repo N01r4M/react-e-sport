93e1d9e23ca58c2a16e95ba5d81a5a3c2aee2a30
Marion Porée <m.poree@outlook.fr>
2022-02-18 14:49:57 +0100

    reset

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import ListVideogames from './components/videogames/ListVideogames';
-import Login from './components/user/Login';
-import VideogameInfo from './components/videogames/VideogameInfo';
 import LeagueInfo from './components/videogames/LeagueInfo';
-import LeagueMatches from './components/videogames/LeagueMatches';
+import LeaguesList from './components/videogames/LeaguesList';
+import Login from './components/user/Login';
 import MatchInfo from './components/videogames/MatchInfo';
+import TeamInfo from './components/videogames/TeamInfo';
+import TournamentInfo from './components/videogames/TournamentInfo';
+import VideogamesList from './components/videogames/VideogamesList';
 
 export default function App() {
     return (
         <Routes>
-            <Route path="/" element={<ListVideogames />} />
-            <Route path="/videogames" element={<ListVideogames />} />
-            <Route path="/videogames/:id" element={<VideogameInfo />} />
+            <Route path="/" element={<VideogamesList />} />
+            <Route path="/videogames" element={<VideogamesList />} />
+            <Route path="/videogames/:id" element={<LeaguesList />} />
             <Route path="/leagues/:id" element={<LeagueInfo />} />
-            <Route path="/leagues/:id/matches/past" element={<LeagueMatches />} />
-            <Route path="/leagues/:id/matches/running" element={<LeagueMatches />} />
-            <Route path="/leagues/:id/matches/upcoming" element={<LeagueMatches />} />
-            <Route path="/leagues/:idLeague/matches/:idMatch" element={<MatchInfo />} />
+            <Route path="/teams/:id" element={<TeamInfo />} />
+            <Route path="/tournaments/:id" element={<TournamentInfo />} />
+            <Route path="/match/:id" element={<MatchInfo />} />
             <Route path="/login" element={<Login />} />
         </Routes>
     )
